155463b10b73c5ac6aca375f4ba1e8e3584ba334
GayoungKim12 <cloud9b58@gmail.com>
2023-02-13 03:01:35 +0900

    문자열이 반복하는 횟수 나타내기

--- coding_test.js
+++ coding_test.js
@@ -1,28 +1,3 @@
-const str = "Hello World";
-if (~str.indexOf("Hey")) {
-  console.log('참');
-} else {
-  console.log('거짓');
-}
-
-const arr = [1, 8, 3];
-if (arr.indexOf((it) => it === 5)) {
-  console.log('참');
-} else {
-  console.log('거짓')
-}
-
-console.log(Math.max(1, 3, 2));
-// Expected output: 3
-
-console.log(Math.max(-1, -3, -2));
-// Expected output: -1
-
-const array1 = [1, 3, 2];
-const array2 = [...array1, 5, 6, 7]
-
-console.log(Math.max(...array2));
-
 function sameStr (strArr) {
   const strSameNum = [];
   const strSet = new Set(strArr);
