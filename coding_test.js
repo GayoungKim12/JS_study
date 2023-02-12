d0c791c5a2fcc89f9e4c44b8af9eb68ee5c6f1ca
GayoungKim12 <cloud9b58@gmail.com>
2023-02-13 03:43:41 +0900

    문자열에서 가장 많이 반복한 문자열 출력

--- coding_test.js
+++ coding_test.js
@@ -1,12 +1,26 @@
-function sameStr (strArr) {
+// 문자열 중 가장 많이 반복하는 알파벳 출력
+function sameStr (s) {
+  let answer = "";
+  const strArr = s.split('').sort((a, b) => a.localeCompare(b));
   const strSameNum = [];
   const strSet = new Set(strArr);
+  const strSetArr = [...strSet];
+
   for (const string of strSet) {
     const strNum = strArr.filter((el) => el === string);
     strSameNum.push(strNum.length);
   }
 
-  return strSameNum;
+  const maxStr = Math.max(...strSameNum); // 배열에서 가장 큰 수 찾기
+
+  for (let i = 0; i < strSameNum.length; i++) {
+    if (strSameNum[i] === maxStr) {
+      answer = strSetArr[i];
+      break;
+    }
+  }
+
+  return answer;
 }
 
-console.log(sameStr(['g', 'g', 'e', 'e', 'g', 'ga', 'a', 'a', 'ga', 'ga', 'ga', 'ga']));
+console.log(sameStr("bbbbbbbbbbbbbbbdhfkahsddddddkjfhkdkkkkkkkkkksdhfjkhskjdhjddddd"));
