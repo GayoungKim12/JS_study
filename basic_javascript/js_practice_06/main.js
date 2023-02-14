94d7b6af78be632c2bcceb876497475debbff1bc
GayoungKim12 <cloud9b58@gmail.com>
2023-02-15 00:48:38 +0900

    js_practice_07 추가

--- basic_javascript/js_practice_06/main.js
+++ basic_javascript/js_practice_06/main.js
@@ -1,18 +1,76 @@
-const obj = {};
-
-let result = '';
-
-try {
-    result = obj.property.a;
-} catch (error) {
-    result = '모름';
-    console.dir(error);
-} finally {
-    console.log(`result = ${result}`);
+// 비동기적 현상
+const A = () => {
+    console.log(`2초 후 출력되는 텍스트`);
+}
+const async = () => {
+    setTimeout(() => {
+        console.log('2초 끝!');
+    }, 2000);
+    A();
 }
 
-try {
-    throw new Error('커스텀 에러');
-} catch(error) {
-    console.dir(error);
+async();// A함수가 실행된 후 setTimeout 함수의 응답을 받음
+
+// Callback 함수
+const B = (sec) => {
+    console.log(`${sec}초 후 출력되는 텍스트`);
+    console.log('Callback 함수 END')
 }
+
+const setTimeoutWithCallback = (callback, delay) => {
+    setTimeout(() => {
+        const delaySec = Number(delay) / 1000;
+        console.log(`${delaySec}초 끝!`);
+        callback(delaySec);
+    }, delay);
+}
+
+setTimeoutWithCallback(B, 4000);
+
+// Promise 함수
+
+const C = (sec) => {
+    console.log(`${sec}초 후 출력되는 텍스트`);
+}
+const setTimeoutWithPromise = (delay) => 
+    new Promise ((resolve) => {
+        setTimeout(() => {
+            const delaySec = Number(delay) / 1000;
+            console.log(`${delaySec}초 끝!`);
+            resolve(delaySec);
+        }, delay); 
+})
+
+setTimeoutWithPromise(6000)
+.then((sec) => {
+    console.log('setTimeoutWithPromise 함수 성공!');
+    return C(sec);
+})
+.then(() => {
+    console.log('C 함수 성공!');
+})
+.finally (() => {
+    console.log('Promise 함수 END');
+})
+
+// async, await 이용
+
+const setTimeoutWithAsync = (delay) => {
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+            const delaySec = Number(delay) / 1000;
+            console.log(`${delaySec}초 끝!`);
+            resolve(delaySec);
+        }, delay); 
+    });
+}
+const D = (sec) => {
+    console.log(`${sec}초 후 출력되는 텍스트`);
+    console.log('Async 함수 END');
+}
+const asyncFunc = async (delay) => {
+    const sec = await setTimeoutWithAsync(delay);
+    D(sec);
+}
+
+asyncFunc(8000);
---
+++ basic_javascript/js_practice_07/main.js
@@ -0,0 +1,15 @@
+fetch("./sample.json")
+.then(async (result) => {
+    console.log('result', result);
+    if (result.ok) {
+        const data = await result.json();
+        console.log(data);
+    }
+});
+
+fetch("https://ohou.se")
+.then((res) => {
+    console.log('success', res);
+}) .catch((err) => {
+    console.log('error', err);
+})
