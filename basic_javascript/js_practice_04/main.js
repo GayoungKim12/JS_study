2c41c2f627d3db82f900afbcf99e4c3e44a37386
GayoungKim12 <cloud9b58@gmail.com>
2023-02-14 16:57:47 +0900

    basic_javascript/js_practice_04 커밋 js_basic_01.js 삭제

---
+++ basic_javascript/js_practice_04/main.js
@@ -0,0 +1,49 @@
+// ❤️ on(이벤트)에 함수 할당
+
+const countUpButton = document.getElementById('count-up-button');
+const count = document.querySelector('span');
+
+// 마우스로 노드를 클릭했을 때 함수 실행
+countUpButton.onclick = (event) => { // 핸들러에 이벤트 객체 전달
+    console.log(event); // 이벤트에 대한 정보 출력
+    if (isNaN(Number(count.innerHTML))) return;
+    count.innerHTML = Number(count.innerHTML) + 1;
+}
+
+// 마우스가 노드 위에 올려졌을 때 요소의 배경색과 글자색이 변경되는 함수 실행
+count.onmouseover = () => {
+    count.style.backgroundColor = "gray";
+    count.style.color = "white";
+}
+
+// 마우스가 노드 위에서 없어졌을 때 요소의 배경색과 글자색이 되돌아오는 함수 실행
+count.onmouseout = () => {
+    count.style.backgroundColor = "transparent";
+    count.style.color = "black";
+}
+
+const input1 = document.querySelectorAll('input')[0];
+
+// 해당 노드에 focus 되었을 때 요소에 자동입력이 값으로 입력되어 있는 함수 실행 
+input1.onfocus = () => {
+    input1.value = '자동입력';
+}
+
+const input2 = document.querySelectorAll('input')[1];
+const result = document.getElementById('result');
+
+// input2의 텍스트가 바뀔 때마다 result의 innerHTML이 동일한 텍스트로 변경되는 함수 실행
+input2.onchange = (event) => {
+    result.innerHTML = event.target.value;
+}
+
+// 💛 addEventListener 이용
+
+const test = document.getElementById('test');
+
+new Array(10).fill(0).forEach((_, index) => {
+    test.addEventListener("click", () => {
+        console.log(`test clicked ${index+1}`);
+    })
+}) // 길이가 10이고, 각각의 요소가 0인 배열 => 각각의 요소가 이벤트인 배열
+
---
+++ basic_javascript/js_practice_04/theory.md
@@ -0,0 +1,38 @@
+# 이벤트(Event)
+
+활동이 발생했다는 신호와 함수(**핸들러**)를 연결시켜 활동 발생 시 해당 함수가 동작하도록(**트리거**되도록) 함
+
+- 사용자에 의해 발생된 이벤트
+  ex) 마우스 클릭, 마우스 이동, 키보드 입력 등
+- 사용자에 의해 발생된 이벤트가 아닌 이벤트
+  ex) 네트워크 연결 실패, 다운로드 완료 등
+
+## 대표적인 DOM 이벤트
+
+- click: 요소를 좌클릭 했을 때 발생
+- mouseover: 요소 위에 마우스를 올렸을 때
+- mouseout: 요소 위에서 마우스를 내렸을 때
+- focus:
+- Blur:
+- keydown:
+- keyup:
+
+## 이벤트와 핸들러 연결
+
+- on(event) 사용
+  - 해당하는 DOM 요소에 event 발생 시 on(event) 함수 실행
+  - <div onclick="함수이름"></div>
+  - Node.onclick = (event) => { 실행할 코드 }
+- addEventListener("event", handler) 사용
+  - 해당 요소에 event 발생 시 handler 실행
+  - Node.addEventListener("click", handler)
+
+## 이벤트 객체
+
+발생한 이벤트에 대한 정보가 담겨 있음
+
+- event.target: 이벤트가 발생한 DOM
+
+## 이벤트 버블링
+
+자식요소에 이벤트가 발생하면 부모요소에 이벤트가 전달되는 현상
