040b45d997014e391c2cebac4ec673884bd947bc
GayoungKim12 <cloud9b58@gmail.com>
2023-03-23 23:19:05 +0900

    미션 레포에서 없애기

--- javascriptMission/05_news-viewer/utils/dom.js
+++
@@ -1,7 +0,0 @@
-export const makeDOMwithProperties = (domType, propertyMap) => {
-  const dom = document.createElement(domType);
-  Object.keys(propertyMap).forEach((key) => {
-    dom[key] = propertyMap[key];
-  });
-  return dom;
-};
